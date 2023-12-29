73ac890d69d18bd3aaa46dd7115d1917856cee40
bbakseong <psm651@naver.com>
2023-12-29 17:47:24 +0900

    init

---
+++ src/test/java/com/company/godsaying/GodsayingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.godsaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodsayingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
